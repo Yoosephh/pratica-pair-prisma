12ccb5adc2260538a8be93da7aab5eff10884c47
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2023-09-21 20:36:38 -0300

    versão final

---
+++ prisma/migrations/20230921231605_/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `cover` to the `books` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "books" ADD COLUMN     "cover" TEXT NOT NULL,
+ALTER COLUMN "purchaseDate" SET DATA TYPE TIMESTAMP(6);
--- src/repositories/books-repository.ts
+++ src/repositories/books-repository.ts
@@ -1,44 +1,62 @@
 import { Book, CreateBook } from "../protocols/book";
 import { CreateReview } from "../protocols/review";
 
-import connection from "../database";
+import prisma from "../database";
 
 export async function getBooks() {
-  const query = `SELECT * FROM books`;
-  const result = await connection.query<Book>(query);
-  return result.rows;
+  const books = await prisma.book.findMany()
+  return books
+  // const query = `SELECT * FROM books`;
+  // const result = await connection.query<Book>(query);
+  // return result.rows;
 }
 
 export async function getBook(id: number) {
-  const query = `SELECT * FROM books WHERE id = $1`;
-  const result = await connection.query<Book>(query, [id]);
-  return result.rows[0];
+  const book = await prisma.book.findUnique({
+    where:{ id }
+  })
+  return book
+  // const query = `SELECT * FROM books WHERE id = $1`;
+  // const result = await connection.query<Book>(query, [id]);
+  // return result.rows[0];
 }
 
 export async function createBook(book: CreateBook) {
-  const { title, author, publisher, purchaseDate } = book;
-  const query = `
-    INSERT INTO books (title, author, publisher, "purchaseDate")
-    VALUES ($1, $2, $3, $4)`;
+  await prisma.book.create({
+    data:book
+  })
 
-  const result = await connection.query(query, [
-    title, author, publisher, purchaseDate
-  ]);
+  // const { title, author, publisher, purchaseDate } = book;
+  // const query = `
+  //   INSERT INTO books (title, author, publisher, "purchaseDate")
+  //   VALUES ($1, $2, $3, $4)`;
 
-  return result.rowCount;
+  // const result = await connection.query(query, [
+  //   title, author, publisher, purchaseDate
+  // ]);
+
+  // return result.rowCount;
 }
 
 export async function reviewBook(bookReview: CreateReview) {
-  const { bookId, grade, review } = bookReview;
-  const query = `
-    UPDATE books 
-    SET
-      grade = $1,
-      review = $2,
-      read = true 
-    WHERE id = $3
-  `;
+  const { bookId: id, grade, review } = bookReview;
+    await prisma.book.update({
+      data: {
+        grade,
+        review,
+        read: true
+      },
+      where: {id}
+    })
+  // const query = `
+  //   UPDATE books 
+  //   SET
+  //     grade = $1,
+  //     review = $2,
+  //     read = true 
+  //   WHERE id = $3
+  // `;
 
-  const result = await connection.query(query, [grade, review, bookId]);
-  return result.rowCount;
+  // const result = await connection.query(query, [grade, review, bookId]);
+  // return result.rowCount;
 }
